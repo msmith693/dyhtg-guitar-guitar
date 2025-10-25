0a02bb245c66ca6cea26596117162149dc6bc4cb
msmith693 <198065835+msmith693@users.noreply.github.com>
2025-10-25 13:17:01 +0100

    Initialise project files

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {useState} from "react";
+
+function App() {
+
+  // [variable, functionToChangeIt] = useState(initialValue)
+  const [name, setName] = useState(""); 
+  return (
+    <div className="p-8 font-sans">
+      <h1 className="text-3xl font-bold text-blue-500 header">Your Instrument Recommender!</h1>
+      <p>Guitar Guitar's personal instrument recommendation tool.</p>
+    <input 
+      type="text"
+      placeholder="Enter your name here"
+      className = "input-field"
+      value = {name}
+      onChange = {(e) => setName(e.target.value)}
+    />
+
+    <p className="mt-4 text-lg">
+      {name ? `Welcome, ${name}!` : "Type your name above."}
+    </p>
+    </div>
+  );
+
+
+}
+
+
+export default App;
